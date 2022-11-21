870b72446ffc27354e3ec261a6f6f12d9497324c
ikleveckas <ignas.kleveckas@gmail.com>
2022-11-21 01:00:11 +0000

    Add files via upload

--- Receiver3.py
+++ Receiver3.py
@@ -18,41 +18,53 @@ class Receiver(object):
     def receive_file(self):
         # create file
         myfile = open(self.file_name, 'wb')
-        print('Receiving file...')
-        ack = (0).to_bytes(2, 'big')
+        #print('Receiving file...')
+        sndpkt = (0).to_bytes(2, 'big')
         # receive file
-        message, sender_address = self.so.recvfrom(1027)
-        while message:
-            # check message seq_no
-            print('Received packet. Seqno {0}. Expected seqno: {1}'.format(int.from_bytes(message[0:2], 'big'), self.expectedseqno))
-            if int.from_bytes(message[0:2], 'big') == self.expectedseqno:
-                # write data
-                myfile.write(message[3:])
-
+        packet, sender_address = self.so.recvfrom(1027)
+        while packet:
+            received_seqno = int.from_bytes(packet[0:2], 'big')
+            # check packet seq_no
+            #print('Received packet. Seqno {0}. Expected seqno: {1}'.format(received_seqno, self.expectedseqno))
+            if received_seqno == self.expectedseqno:
                 # send ack
-                ack = self.expectedseqno.to_bytes(2, 'big')
-                self.so.sendto(ack, sender_address)
+                sndpkt = self.expectedseqno.to_bytes(2, 'big')
+                self.so.sendto(sndpkt, sender_address)
+
+                # write data
+                myfile.write(packet[3:])
 
                 # update sequence_no
                 self.expectedseqno += 1
 
                 # check if end of file
-                if message[2] == 1:
-                    print('Received final packet. Download finished.')
+                if packet[2] == 1:
+                    #print('Received final packet. Download finished.')
                     # send more acks as per instructions in question @152 of comn piazza 
                     for i in range(15):
-                        ack = self.expectedseqno.to_bytes(2, 'big')
-                        self.so.sendto(ack, sender_address)
+                        #sndpkt = self.expectedseqno.to_bytes(2, 'big')
+                        self.so.sendto(sndpkt, sender_address)
+                    
+                    while True:
+                        self.so.settimeout(2)
+                        try:
+                            packet, sender_address = self.so.recvfrom(1027)
+                            received_seqno = int.from_bytes(packet[0:2], 'big')
+                            #print('!! received a packet with seqno', received_seqno)
+                            #ack = (self.expectedseqno).to_bytes(2, 'big')
+                            self.so.sendto(sndpkt, sender_address)
+                        except timeout:
+                            break
                     break
             
             # handle all other cases
             else:
                 # send ack
                 #ack = (self.expectedseqno).to_bytes(2, 'big')
-                self.so.sendto(ack, sender_address)
-                print("Sent ACK with seqno ", int.from_bytes(ack, 'big'))
+                self.so.sendto(sndpkt, sender_address)
+                #print("Sent ACK with seqno ", int.from_bytes(ack, 'big'))
             # get next message
-            message, sender_address = self.so.recvfrom(1027)
+            packet, sender_address = self.so.recvfrom(1027)
         
         # close receiver
         myfile.close()
--- Sender3.py
+++ Sender3.py
@@ -1,10 +1,10 @@
-# Ignas Kleveckas s2095960
+# Ignas Kleveckas S2095960
+
 import sys
-from socket import *
-from threading import Thread
-from threading import Lock
-from typing import OrderedDict
+import time
+import os
 import select
+from socket import *
 
 class Sender(object):
     def __init__(self, remote_host, port, file_name, retry_timeout, window_size):
@@ -17,161 +17,109 @@ class Sender(object):
 
         # configure socket
         self.so = socket(AF_INET, SOCK_DGRAM)
-        self.so.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
         # initialise remaining values
-        self.sequence_no = 0
-        self.rentransmissions = 0
+        self.sequence_no = 1
+        self.window_base = 1
         self.transfer_time = 0
-        self.file_size = 0
-    
-    def send_file(self):
-        lock = Lock()
-        window = Window(self.window_size, lock)
-        packet_sender = PacketSender(self.file_name, self.so, (self.remote_host, self.port),
-        window, self.retry_timeout, lock)
-        ack_receiver = AckReceiver(self.so, window, self.retry_timeout, lock)
-        packet_sender.start()
-        ack_receiver.start()
-        packet_sender.join()
-        ack_receiver.join()
-        self.so.close()
-
-
-class Window(object):
-    def __init__(self, window_size, lock):
-        self.window_size = window_size
-        self.finished = False
-        self.base = 1
-        self.nextseqnum = 1
-        self.lock = lock
-    
-    def empty(self):
-        return self.base == self.nextseqnum
-
-    def full(self):
-        return self.nextseqnum >= self.base + self.window_size
-
-    def ack(self, seq_no):
-        self.lock.acquire()
-        self.base = seq_no + 1
-        self.lock.release()
-
-    def clear(self):
-        self.nextseqnum = self.base
-
-
-class PacketSender(Thread):
-    def __init__(self, file_name, socket, receiver_address, window, timeout, lock):
-        self.file_name = file_name
+        self.file_size = os.path.getsize(file_name)
         self.buffer_length = 1024
-        self.file_size = 0
-        self.so = socket
-        self.receiver_address = receiver_address
-        self.window = window
-        self.timeout = timeout
-        self.lock = lock
-        Thread.__init__(self)
-
-    def run(self):
-        packets = self.read_file()
-        packet = packets[0] # might be empty file check if raises an error!
-        print('Started sending')
-        while True:
-            if self.window.base >= len(packets):
-                self.window.finished = True
-                break
-            if self.window.full() or self.window.nextseqnum >= len(packets):
-                continue
-            self.lock.acquire()
-            packet = packets[self.window.nextseqnum]
-            if self.window.empty() or not self.so.gettimeout():
-                self.so.settimeout(self.timeout)    
-                print('Timer set from nothing!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!.')
-            print('Packet sent with seqnum {0}, while base is {1}'.format(self.window.nextseqnum, self.window.base))
-            self.so.sendto(packet, self.receiver_address)
-            self.window.nextseqnum += 1
-            self.lock.release()
-        print('Finished sending')
-
-
-    def read_file(self):
-        myfile = open(self.file_name, 'rb')
-        i = 1
-        packets = [bytearray((0).to_bytes(1, 'big'))] #dummy value in 0 position
-        while True:
-            data = myfile.read(self.buffer_length)
-            if len(data) == 0:
-                break
-            self.file_size += len(data)
-            if len(data) < self.buffer_length:
-                end_of_file = 1
-            else:
-                end_of_file = 0
-            packet = bytearray((i).to_bytes(2, 'big'))
-            packet.extend(end_of_file.to_bytes(1, 'big'))
-            packet.extend(data)
-            packets.append(packet)
-            i += 1
-        
-        return packets
+        self.total_packets = 1 + (self.file_size / self.buffer_length)
+        self.timeout = False
+        self.timer = False
+        self.packets = [bytearray((0).to_bytes(1, 'big'))] #dummy value in 0 position
         
 
+    def packet_monitor(self):
+        if not self.timeout:
+            time_left = self.retry_timeout - (time.time() - self.time_sent)
+            if time_left >= 0:
+                
+                # check if there is an incoming packet
+                ready = select.select([self.so], [], [], 0)
+                if ready[0]:
+                    self.receive()
+            else:
+                self.timeout = True
+        else:
+            self.process_timeout()
 
+    def receive(self):
+        response, addr = self.so.recvfrom(self.buffer_length)
+        ack_no = int.from_bytes(response, 'big')
+        #print("ACK received " + str(ack_no))
+        self.window_base = ack_no + 1
+
+        # if the ack for the base packet was received, stop the timer
+        if self.window_base == self.sequence_no:
+            self.timer = False
+
+        # otherwise reset the timer
+        else:
+            self.time_sent = time.time()
+            self.timer = True
+
+    def process_timeout(self):
+        self.timer = True
+        self.timeout = False
+        self.time_sent = time.time()
+
+        # resend the packets in the window (after window base)
+        for i in range(self.window_base, self.sequence_no):
+            packet = self.packets[i]
+            self.so.sendto(packet, (self.remote_host, self.port))
+            #print("Packet retransmitted " + str(i))
             
 
-class AckReceiver(Thread):
-    def __init__(self, so, window, timeout, lock):
-        self.so = so
-        self.window = window
-        self.timeout = timeout
-        self.lock = lock
-        Thread.__init__(self)
+    def send_file(self):
+        myfile = open(self.file_name, "rb")
+        data = myfile.read(self.buffer_length)
+        end_of_file = 0
+        start = time.time()
+        while self.window_base < self.total_packets or data:
+            #if time.time() - start > 180: # TESTING
+                #break
+            if self.sequence_no < self.window_base + self.window_size and data:
 
-    def run(self):
-        while not self.window.finished:
-            if self.window.empty():
-                continue
+                # prepare header
+                if len(data) < self.buffer_length:
+                    end_of_file = 1
+                else:
+                    end_of_file = 0
+                packet = bytearray(self.sequence_no.to_bytes(2, 'big'))
+                packet.extend(end_of_file.to_bytes(1, 'big'))
+                packet.extend(data)
 
-            if self.window.full():
-                self.lock.acquire()
-                self.listen()
-                self.lock.release()
-            
-            else:
-                self.listen()
+                # send packet and add it to the packet list
+                self.packets.append(packet)
+                self.so.sendto(packet, (self.remote_host, self.port))
+                #print("Packet sent " + str(self.sequence_no))
 
+                # if sending the base packet, start the timer
+                if self.window_base == self.sequence_no:
+                    self.time_sent = time.time()
+                    self.timer = True
 
-    def listen(self):
-        try:
-            response, addr = self.so.recvfrom(2)
-            self.window.base = int.from_bytes(response[0:2], 'big') + 1
-            print('Received ACK with seq no {0}. New base = {1}'.format(int.from_bytes(response[0:2], 'big'), self.window.base))
-            if self.window.empty():
-                print('Timer stopped.')
-                self.so.settimeout(None)
-            else:
-                print('Timer set.')
-                self.so.settimeout(self.timeout)
-        except timeout:
-            print('Timeout. Resetting timer.')
-            self.so.settimeout(self.timeout)
-            self.window.clear()
-        
-            """
-            
-            ready = select.select([self.so], [], [], self.timeout)
-            if not ready[0]:
-                self.window.clear()
-                continue
+                self.sequence_no += 1
 
-            else:
-                response, address = self.so.recvfrom(2)
-                response_seq_no = int.from_bytes(response[0:2], 'big')
-                self.window.ack(response_seq_no)
+                # read data for the next packet
+                data = myfile.read(self.buffer_length)
+                if len(data) < self.buffer_length:
+                    end_of_file = 1
 
-            """
+            if self.timer:
+                self.packet_monitor()
+
+        total_time = time.time() - start
+        throughput = self.file_size / (1000 * total_time)
+        print(throughput)
+
+        # close the file and socket
+        self.so.close()
+        myfile.close()
+
+        return throughput
+
 if __name__ == '__main__':
     sender = Sender(sys.argv[1], int(sys.argv[2]), sys.argv[3], int(sys.argv[4]), int(sys.argv[5]))
     sender.send_file()
-
